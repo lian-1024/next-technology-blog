2f2168d41859e9cf2470106459f0406f7716c1e8
lian-1024 <2088486180@qq.com>
2024-09-22 18:33:46 +0800

    feat: 笔记组件初始样式

--- src/app/(root)/note/page.tsx
+++ src/app/(root)/note/page.tsx
@@ -1,5 +1,126 @@
+import NoteCard from '@/components/note/note-card';
+import NoteNavigation from '@/components/note/note-navigation';
+
+const CardList = [
+  {
+    id: 1,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+  {
+    id: 2,
+    title: 'NextJS实现微信扫码登录',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nihil, officia magni officiis aut, placeat doloribus, vero expedita eaque unde architecto autem ullam at ratione nemo dolorem! Sequi, aperiam blanditiis.',
+    view: 100,
+    tag: ['前端', '全栈', 'NextJS', '登录'],
+  },
+];
+
 const Note = () => {
-  return <div className="pt-[56px]">Note</div>;
+  return (
+    <div className="pt-[86px] p-20 flex gap-12 relative">
+      <main className="p-5">
+        <NoteCard cardList={CardList} />
+      </main>
+      <aside className="flex-1 min-w-36 h-fit">
+        <div className="fixed z-50 border border-slate-100 rounded-xl overflow-hidden h-fit ">
+          <NoteNavigation />
+        </div>
+      </aside>
+    </div>
+  );
 };
 
 export default Note;
---
+++ src/components/note/note-navigation.tsx
@@ -0,0 +1,42 @@
+import { Code } from 'lucide-react';
+import Link from 'next/link';
+
+const CategoryMenu = [
+  {
+    id: 1,
+    name: '前端',
+  },
+  {
+    id: 2,
+    name: '后端',
+  },
+  {
+    id: 3,
+    name: '全栈',
+  },
+  {
+    id: 4,
+    name: '前沿技术',
+  },
+  {
+    id: 5,
+    name: 'AI',
+  },
+];
+
+const NoteNavigation = () => {
+  return (
+    <ul>
+      {CategoryMenu.map((item) => (
+        <li key={item.id} className="hover:bg-slate-50 px-6">
+          <Link href={'/'} className="h-12 leading-[3rem] flex items-center">
+            <Code className="mr-2 h-4 w-4 text-slate-600" />
+            <span className="text-sm text-slate-600">{item.name}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NoteNavigation;
